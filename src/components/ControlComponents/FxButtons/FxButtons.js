65a4b7461a34a64fba72f68f2e9b95081d01dc2b
Alex Younger <alexanderyounger@gmail.com>
2021-02-01 15:46:03 -0500

    fixes fx buttons

--- src/components/ControlComponents/FxButtons/FxButtons.js
+++ src/components/ControlComponents/FxButtons/FxButtons.js
@@ -4,6 +4,18 @@ import ReactAudioPlayer from 'react-audio-player';
 import { StoreContext } from '../../../store/context';
 import { actions } from '../../../store/actions';
 
+function importAll(r) {
+	const fx = {};
+	r.keys().forEach((item) => {
+		fx[item.replace('./', '')] = r(item);
+	});
+	return fx;
+}
+
+const buttonFiles = importAll(
+	require.context('../../../assets/fx_buttons', false, /\.mp3$|.wav$|.mp4$/)
+);
+
 export default function FxButtons({ changeFX, toggleDevTools }) {
 	const { state, dispatch } = useContext(StoreContext);
 	const { audio, fxButtons, VFX } = state;
@@ -27,7 +39,7 @@ export default function FxButtons({ changeFX, toggleDevTools }) {
 			? dispatch({ type: actions.END_VIDEO })
 			: dispatch({
 					type: actions.PLAY_VIDEO,
-					payload: `fx_buttons/${button.file}`,
+					payload: buttonFiles[button.file],
 			  });
 	};
 
@@ -53,7 +65,11 @@ export default function FxButtons({ changeFX, toggleDevTools }) {
 						<Text>{button.name}</Text>
 						{button.type === 'audio' ? (
 							<ReactAudioPlayer
-								src={`fx_buttons/${button.file}`}
+								src={
+									button.file.slice(6) === 'app://'
+										? button.file
+										: buttonFiles[button.file]
+								}
 								volume={(audio.volume.master / 100) * (audio.volume.sfx / 100)}
 							/>
 						) : null}
