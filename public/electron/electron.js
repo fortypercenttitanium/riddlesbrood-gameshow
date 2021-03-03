e91d9983c921483389d101b7cea1dc3a0b6c9145
Alex Younger <alexanderyounger@gmail.com>
2021-03-03 16:04:29 -0500

    adding autoUpdater features

--- public/electron/electron.js
+++ public/electron/electron.js
@@ -1,9 +1,13 @@
 const electron = require('electron');
+const { autoUpdater } = require('electron-updater');
 const path = require('path');
 const createStartScreen = require('./electronHelpers/createStartScreen');
 const attachIPCListeners = require('./electronHelpers/attachIPCListeners');
 const checkAssetFolders = require('./electronHelpers/checkAssetFolders');
 const { protocol, app } = electron;
+
+autoUpdater.logger = require('electron-log');
+autoUpdater.logger.transports.file.level = 'info';
 
 let mainWindow;
 let gameWindow;
@@ -49,12 +53,13 @@ checkAssetFolders();
 attachIPCListeners({ getWindow, setWindow });
 
 app.on('ready', () => {
+	autoUpdater.checkForUpdatesAndNotify();
 	protocol.registerFileProtocol('app', (req, cb) => {
 		const url = req.url.substr(6);
 		const userDataPath = app.getPath('userData');
 		cb({ path: path.join(userDataPath, url) });
 	});
-	createStartScreen({ setWindow });
+	createStartScreen({ setWindow, autoUpdater });
 });
 
 app.on('window-all-closed', () => {
